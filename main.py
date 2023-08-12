4256726f58201d411ff39b4a5a61991f2139f9e9
alpertunga-bile <kingdom19online@hotmail.com>
2023-08-12 15:28:56 +0300

    prepare for default

--- main.py
+++ main.py
@@ -11,26 +11,11 @@ def main_train():
     train_args.auto_find_batch_size = True
     train_args.torch_compile = True
 
-    train(
-        "positive_female_pruned.txt",
-        "female_positive_generator_sfw_onnx",
-        "tmp",
-        train_args=train_args,
-    )
-
 
 def main_generate():
     default_args = GenerateArgs()
     default_args.min_length = 30
     default_args.max_length = 75
-
-    print(
-        generate_text(
-            input="goddess, angel, female",
-            model="female_positive_generator_sfw_onnx",
-            args=default_args,
-        )
-    )
 
 
 if __name__ == "__main__":
